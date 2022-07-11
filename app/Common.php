732e28ebaf2614b3f0a16c5cdb6d2f92d006bcb1
KiXcellent <francis.s.crisostomo@gmail.com>
2022-07-11 08:17:23 +0800

    add View Library to override default view method

--- app/Common.php
+++ app/Common.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Libraries\View\ViewName;
+
 /**
  * The goal of this file is to allow developers a location
  * where they can overwrite core procedural functions and
@@ -13,3 +15,33 @@
  *
  * @see: https://codeigniter4.github.io/CodeIgniter4/
  */
+
+ if (! function_exists('view')) {
+    /**
+     * Grabs the current RendererInterface-compatible class
+     * and tells it to render the specified view. Simply provides
+     * a convenience method that can be used in Controllers,
+     * libraries, and routed closures.
+     *
+     * NOTE: Does not provide any escaping of the data, so that must
+     * all be handled manually by the developer.
+     *
+     * @param array $options Unused - reserved for third-party extensions.
+     */
+    function view(string $name, array $data = [], array $options = []): string
+    {
+        /**
+         * @var CodeIgniter\View\View $renderer
+         */
+        $renderer = Services::renderer();
+
+        $saveData = config(View::class)->saveData;
+
+        if (array_key_exists('saveData', $options)) {
+            $saveData = (bool) $options['saveData'];
+            unset($options['saveData']);
+        }
+        $view = ViewName::normalize($view);
+        return $renderer->setData($data, 'raw')->render($name, $options, $saveData);
+    }
+}
